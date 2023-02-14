de6c52d148e6b4dad7b71bb801fa984719076983
aldiemhaaa <aldi.emha@gmail.com>
2023-02-14 15:18:08 +0700

    perbaikan

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Administrator',
+  email: 'demo@admin.com',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
